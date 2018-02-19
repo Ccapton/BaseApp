bfe61fcd5a0241011f9c18bc86315c37fd6a2f78
ccapton <chenweibin1125@foxmail.com>
2018-02-19 23:32:04 +0800

    添加AndroidEventBus框架 https://github.com/hehonghui/AndroidEventBus

--- app/src/main/java/com/capton/baseapp/MainActivity.java
+++ app/src/main/java/com/capton/baseapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
-import com.blankj.utilcode.util.FragmentUtils;
 import com.capton.baseapp.databinding.ActivityMainBinding;
 import com.capton.common.base.BaseActivity;
 
@@ -16,7 +15,6 @@ import java.util.Map;
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
 
-    AnimationFragment animationFragment;
 
     @Override
     public String[] getPermissions() {
@@ -45,25 +43,6 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-          animationFragment = new AnimationFragment();
-          AnimationChildFragment animationChildFragment = new AnimationChildFragment();
-          Bundle bundle = new Bundle();
-          bundle.putInt("type",0);
-          animationChildFragment.setArguments(bundle);
-
-          AnimationChildFragment animationChildFragment2 = new AnimationChildFragment();
-          Bundle bundle2 = new Bundle();
-          bundle2.putInt("type",1);
-          animationChildFragment2.setArguments(bundle2);
-
-          fragmentList.add(animationChildFragment);
-          fragmentList.add(animationChildFragment2);
-          animationFragment.setFragmentList(fragmentList);
-          titleList.add("连载");
-          titleList.add("完结");
-          animationFragment.setFragmentNameList(titleList);
-
-         FragmentUtils.add(getSupportFragmentManager(),animationFragment,R.id.fragmentFrameLayout);
 
      }
 
