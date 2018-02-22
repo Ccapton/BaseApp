03bd469e37637fb8cd4b3752b63a72fdc8a4e498
ccapton <chenweibin1125@foxmail.com>
2018-02-22 22:27:31 +0800

    添加Lottie动画框架

--- app/src/main/java/com/capton/baseapp/MainActivity.java
+++ app/src/main/java/com/capton/baseapp/MainActivity.java
@@ -1,18 +1,10 @@
 package com.capton.baseapp;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 
 import com.capton.baseapp.databinding.ActivityMainBinding;
 import com.capton.common.base.BaseActivity;
-
-import net.steamcrafted.loadtoast.LoadToast;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
@@ -34,25 +26,13 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
     }
 
-    LoadToast lt;
+
     @Override
     public void clickRightText() {
-        if (lt == null) {
-            lt = new LoadToast(this);
-            lt.setText("数据加载中");
-            lt.setTextSize(12);
-            lt.setBackgroundColor(Color.GRAY);
-            lt.setTranslationY(300);
-            lt.show();
-        }else {
-            lt.success();
-            lt = null;
-        }
+        binding.animationView.setAnimation("atm.json");
+        binding.animationView.loop(true);
+        binding.animationView.playAnimation();
     }
-
-    List<Fragment> fragmentList=new ArrayList<>();
-    List<String> titleList=new ArrayList<>();
-
       @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,8 +40,6 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
      }
 
-    Map<Integer,Integer> resIdMap;
-    List<String> data;
     @Override
     protected void yourOperation() {
         setTitle(R.string.app_name);
